be5928a0fb453ffdc1130a4b2b83ba91faf04935
lch8798 <lch8798@gmail.com>
2020-11-04 10:56:44 +0900

    add. static server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+// server config
+const port = 80;
+
+
+// client-side react render
+app.use(express.static(__dirname + '/build'));
+app.listen(port, () => {
+    console.log(`run node server port: ${port}`);
+});
